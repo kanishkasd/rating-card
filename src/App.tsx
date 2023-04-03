3f742fbd780e754e3f6a0f1c3b7dfd169f6356b1
kanishka <kanishkasandaru2048@gmail.com>
2023-04-03 19:26:25 +0530

    rating-card

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Rating } from "./components/Rating";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Rating />
+      </div>
+    </div>
+  );
+}
+
+export default App;
